galactics/beyond

commit 9091bf812bfed5092b07a2a05a9f3afbbc73a2ca

fix encoding : don't rely on default platform encoding for known file
Provide encoding parameters for external files, with default to ascii

close #33

beyond/env/jpl.py
@@ -183,7 +183,7 @@ class Pck(dict):
                 log.warning(f"File not found : {filepath}")
                 continue
 
-            with filepath.open() as fp:
+            with filepath.open(encoding="ascii") as fp:
                 lines = fp.read().splitlines()
 
             datablock = False
@@ -444,7 +444,7 @@ if __name__ == "__main__":  # pragma: no cover
     for file in sys.argv[1:]:
         print(file)
         print("*" * len(file))
-        for segment in SPK.open(file).segments:
+        for segment in SPK.open(file, encoding="ascii").segments:
 
             start = Date(segment.start_jd - Date.JD_MJD)
             end = Date(segment.end_jd - Date.JD_MJD)

beyond/dates/eop.py
@@ -24,12 +24,12 @@ class TaiUtc:
     This file could be retrieved `here <http://maia.usno.navy.mil/ser7/tai-utc.dat>`__, but this server seems discontinued.
     """
 
-    def __init__(self, path):
+    def __init__(self, path, encoding="ascii"):
 
         self.path = Path(path)
         self.data = []
 
-        with self.path.open() as fhandler:
+        with self.path.open(encoding=encoding) as fhandler:
             lines = fhandler.read().splitlines()
 
         for line in lines:
@@ -79,12 +79,12 @@ class Finals2000A:
 
     deltas = ("dx", "dy")
 
-    def __init__(self, path):
+    def __init__(self, path, encoding="ascii"):
 
         self.path = Path(path)
         d1, d2 = self.deltas
 
-        with self.path.open() as fp:
+        with self.path.open(encoding=encoding) as fp:
             lines = fp.read().splitlines()
 
         self.data = {}

beyond/frames/iau1980.py
@@ -19,7 +19,7 @@ def _tab(max_i=None):
     filepath = Path(__file__).parent / "data" / "tab5.1.txt"
 
     result = []
-    with filepath.open() as fhd:
+    with filepath.open(encoding="utf-8") as fhd:
         i = 0
         for line in fhd.read().splitlines():
             if line.startswith("#") or not line.strip():

beyond/frames/iau2010.py
@@ -24,7 +24,7 @@ def _tab():
         filepath = Path(__file__).parent / "data" / element
 
         total = []
-        with filepath.open() as fhd:
+        with filepath.open(encoding="ascii") as fhd:
 
             for line in fhd.read().splitlines():
 

commit 00b51c19676fb16a4ba977bc5790da1bbbe75e78

fix encoding : bug introduced in 9091bf8
SPK file are binary, duh !

beyond/env/jpl.py
@@ -444,7 +444,7 @@ if __name__ == "__main__":  # pragma: no cover
     for file in sys.argv[1:]:
         print(file)
         print("*" * len(file))
-        for segment in SPK.open(file, encoding="ascii").segments:
+        for segment in SPK.open(file).segments:
 
             start = Date(segment.start_jd - Date.JD_MJD)
             end = Date(segment.end_jd - Date.JD_MJD)